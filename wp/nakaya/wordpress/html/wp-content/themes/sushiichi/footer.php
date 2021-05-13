96985a7ec8c1ecc16d576168c764f2380dea978c
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2021-05-13 22:24:33 +0900

    鮨いちのトップページをワードプレス化

---
+++ wp/nakaya/wordpress/html/wp-content/themes/sushiichi/footer.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ Template Name :Sushiichi
+*/
+
+?>
+     <div id="footer__content">
+         <div class="footer__image"></div>
+         <div class="footer__top"></div>
+
+        <div class="footer__bottom">
+          <div class="footer__kuuhaku"></div>
+	  
+          <div class="footer__bottom__text">
+            <p>東中野に根差し地域と人に愛される鮨屋へ</p>
+          </div>
+
+          <div class="footer__lists">
+            <ul>
+              <li class="store__name"><a href="#wrapper">鮨いち</a></li>
+              <li><a href="#wrapper">トップ</a></li>
+              <li><a href="sinagaki.php">お品書き</a></li>
+              <li><a href="sousaku.php">創作</a></li>
+              <li><a href="form.php">店舗情報</a></li>
+            </ul>
+          </div>
+
+        </div>
+     </div>
+
---
+++ wp/nakaya/wordpress/html/wp-content/themes/sushiichi/header.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ Template Name :Sushiichi
+*/
+?>
+      <div id="header__content">
+        <div class="header__top">
+          <div class="header__top__left">
+            <h1><a href="index.php">鮨いち</a></h1>
+              <div class="menu__content">
+                <h3 class="menu__icon"><div></div><div></div><div></div></h3>
+                 <ul id="menu__content__list">
+                  <li><a href="index.php">トップ</a></li>
+                  <li><a href="sousaku.php">創作</a></li>
+                  <li><a href="sinagaki.php">お品書き</a></li>
+                  <li><a href="form.php">アクセス</a></li>
+                  <li><a href="form.php">お問い合わせ</a></li>
+                 <div id="close__btn">
+                  <p>閉じる</p>
+                 </div>
+                </ul>
+              </div>
+          </div>
+
+          <div class="header__top__right">
+            <ul>
+              <li class="top__name"><a href="index.php">トップ</a></li>
+              <li><a href="<?php echo home_url("sousaku.php"); ?>">創作</a></li>
+              <li><a href="sinagaki.php">お品書き</a></li>
+              <li><a href="form.php">アクセス</a></li>
+              <li><a href="form.php">お問い合わせ</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="header__bottom">
+          <div class="header__bottom__left">
+            <p>あたたかいおもてなしをモットーに<br>
+              魚それぞれの良さを活かした鮓を握っています</p>
+            <b>@東中野xx年</b>
+          </div>
+          <div class="header__bottom__right">
+            <div class="header__image"></div>
+          </div>
+
+        </div>
+      </div> 
